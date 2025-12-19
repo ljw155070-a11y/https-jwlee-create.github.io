d63ffe10a37cda79aefbe5fbd981152fbb7dd7f1
ljw155070-a11y <ljw155070@gmail.com>
2025-12-19 16:10:22 +0900

    Deploy from a branch

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", //  레포 이름과 완전히 동일해야 함
+  base: "/https-jwlee-create.github.io/", //  레포 이름과 완전히 동일해야 함
   build: {
     outDir: "docs",
     emptyOutDir: true, // 매우 중요! 이전 파일 완전히 삭제
