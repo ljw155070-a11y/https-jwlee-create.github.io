8d9dfa37d9d4b0df5856b27b56578cf026ed41c6
ljw155070-a11y <ljw155070@gmail.com>
2025-12-19 16:11:36 +0900

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https-jwlee-create.github.io/", //  레포 이름과 완전히 동일해야 함
+  base: "/https-jwlee-create.github.io/",
   build: {
     outDir: "docs",
-    emptyOutDir: true, // 매우 중요! 이전 파일 완전히 삭제
+    emptyOutDir: true,
   },
 });
