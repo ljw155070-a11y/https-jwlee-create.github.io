5f8725ae671f550a48d80121c9152ef8d9e886bf
ljw155070-a11y <ljw155070@gmail.com>
2025-12-19 16:05:57 +0900

    build for pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jinwon-portfolio/", //  레포 이름과 완전히 동일해야 함
+  base: "/", //  레포 이름과 완전히 동일해야 함
   build: {
     outDir: "docs",
     emptyOutDir: true, // 매우 중요! 이전 파일 완전히 삭제
